935170cb126670b13d0ecf0b621ae60b2d72b10e
Caitlin Millar <caitlin-millar@outlook.com>
2022-12-01 15:11:34 +0000

    added enclosures

--- safari.sql
+++ safari.sql
@@ -31,10 +31,19 @@ CREATE TABLE assignments (
     employeeId INT REFERENCES employees(id)
 );
 
+INSERT INTO employees (name, employeeNumber) VALUES ('Captain Rick', 12345);
 INSERT INTO employees (name, employeeNumber) VALUES ('John Smith', 9832);
 INSERT INTO employees (name, employeeNumber) VALUES ('Mary Smith', 9234);
 INSERT INTO employees (name, employeeNumber) VALUES ('Alex Smith II', 9128);
-INSERT INTO employees (name, employeeNumber) VALUES ('Bav Bav', 2738);
+INSERT INTO employees (name, employeeNumber) VALUES ('Bav Bav Snake King', 2738);
 INSERT INTO employees (name, employeeNumber) VALUES ('Blue Millar', 3546);
 INSERT INTO employees (name, employeeNumber) VALUES ('Steve Irwin', 5623);
 
+
+
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('big cat field', 20, FALSE);
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('elephant hang out spot', 100, TRUE);
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('monkey jungle', 5000, FALSE);
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('penguins ice rink', 4, FALSE);
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('giraffe living room', 13, FALSE);
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('snake pit', 89, FALSE);

ed2f162bc9856ede2affbc63051cecbef35314fe
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2022-12-01 16:34:17 +0000

    fixed push

--- safari_queries.sql
+++ safari_queries.sql
@@ -6,3 +6,25 @@ SELECT * FROM employees
 INNER JOIN assignments 
 ON employees.id = assignments.employeeId
 WHERE assignments.enclosureId = 3;
+
+-- The names of the staff working in enclosures which are closed for maintenance
+SELECT DISTINCT employees.name FROM employees
+INNER JOIN assignments 
+ON employees.id = assignments.employeeId
+INNER JOIN enclosures
+ON enclosures.id = assignments.enclosureid
+WHERE enclosures.closedformaintenance = TRUE;
+
+--The name of the enclosure where the oldest animal lives. If there are two animals who are the same age choose the first one alphabetically.
+SELECT enclosures.name FROM enclosures 
+INNER JOIN animals
+ON enclosures.id = animals.enclosureid
+ORDER BY animals.age DESC, animals.name LIMIT 1;
+
+--The number of different animal types a given keeper has been assigned to work with.
+
+
+--The number of different keepers who have been assigned to work in a given enclosure
+
+
+--The names of the other animals sharing an enclosure with a given animal (eg. find the names of all the animals sharing the big cat field with Tony)
